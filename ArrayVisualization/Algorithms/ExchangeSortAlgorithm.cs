d93286321fb78b25509e3b3cbbd9d187420ac202
Halid Odat <halidodat@gmail.com>
2022-06-27 14:31:46 -0700

    Implement Exchange Sort Algorithm (#7)

---
+++ ArrayVisualization/Algorithms/ExchangeSortAlgorithm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayVisualization.Algorithms
+{
+    public class ExchangeSortAlgorithm : Algorithm
+    {
+        public ExchangeSortAlgorithm(Array array) : base(array)
+        {
+        }
+
+        protected override IEnumerator<AlgorithmState> CreateEnumerator()
+        {
+			for (int i = 0; i < Array.Count - 1; ++i)
+			{
+				yield return new AlgorithmState(new List<int> { i });
+				for (int j = i + 1; j < Array.Count; ++j)
+				{
+					yield return new AlgorithmState(new List<int> { i, j });
+					if (Array[i] > Array[j])
+					{
+						Array.Swap(i, j);
+						yield return new AlgorithmState(new List<int> { i, j });
+					}
+				}
+			}
+		}
+    }
+}
