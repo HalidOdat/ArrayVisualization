b7a01903d4f6f91890106e35029d6d51a49762aa
HalidOdat <halidodat@gmail.com>
2022-06-27 22:42:57 +0200

    Implement Insertion Sort Algorithm

---
+++ ArrayVisualization/Algorithms/InsertionSortAlgorithm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayVisualization.Algorithms
+{
+    public class InsertionSortAlgorithm : Algorithm
+    {
+        public InsertionSortAlgorithm(Array array) : base(array)
+        {
+        }
+
+        protected override IEnumerator<AlgorithmState> CreateEnumerator()
+        {
+            for (int i = 0; i < Array.Count - 1; i++)
+            {
+                yield return new AlgorithmState(new List<int> { i });
+                for (int j = i + 1; j > 0; j--)
+                {
+                    yield return new AlgorithmState(new List<int> { i, j });
+                    if (Array[j - 1] > Array[j])
+                    {
+                        Array.Swap(j - 1, j);
+                        yield return new AlgorithmState(new List<int> { i, j, j - 1 });
+                    }
+                }
+            }
+
+            yield break;
+        }
+    }
+}
