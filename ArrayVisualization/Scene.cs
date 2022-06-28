cd8d487627e8f405517546a72f50c70ac03f9d57
HalidOdat <halidodat@gmail.com>
2022-06-28 09:51:05 +0200

    Feature color mode for Bar and Dot representation

--- ArrayVisualization/Scene.cs
+++ ArrayVisualization/Scene.cs
@@ -44,6 +44,8 @@ namespace ArrayVisualization
 
         public bool BarMode { get; set; } = true;
 
+        public bool Colored { get; set; } = false;
+
         public void SetAlgorithm(string type)
         {
             this.Array.Accesses = 0;
@@ -120,9 +122,101 @@ namespace ArrayVisualization
             }
         }
 
+        // This function will take an HSL value in the ranges of 0-1 and return a color object.  I use this function all the time.
+        // Given H,S,L in range of 0-1
+        // Returns a Color (RGB struct) in range of 0-255
+        public static Color HSL2RGB(double h, double sl, double l)
+        {
+            double v;
+            double r, g, b;
+
+            r = l;   // default to gray
+            g = l;
+            b = l;
+            v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
+
+            if (v > 0)
+            {
+                double m;
+                double sv;
+                int sextant;
+                double fract, vsf, mid1, mid2;
+
+                m = l + l - v;
+                sv = (v - m) / v;
+                h *= 6.0;
+                sextant = (int)h;
+                fract = h - sextant;
+                vsf = v * sv * fract;
+                mid1 = m + vsf;
+                mid2 = v - vsf;
+
+                switch (sextant)
+                {
+                    case 0:
+                        r = v;
+                        g = mid1;
+                        b = m;
+                        break;
+                    case 1:
+
+                        r = mid2;
+
+                        g = v;
+
+                        b = m;
+
+                        break;
+
+                    case 2:
+
+                        r = m;
+
+                        g = v;
+
+                        b = mid1;
+
+                        break;
+
+                    case 3:
+
+                        r = m;
+
+                        g = mid2;
+
+                        b = v;
+
+                        break;
+
+                    case 4:
+
+                        r = mid1;
+
+                        g = m;
+
+                        b = v;
+
+                        break;
+
+                    case 5:
+
+                        r = v;
+
+                        g = m;
+
+                        b = mid2;
+
+                        break;
+
+                }
+
+            }
+
+            return Color.FromArgb(Convert.ToByte(r * 255.0f), Convert.ToByte(g * 255.0f), Convert.ToByte(b * 255.0f));
+        }
+
         public void Draw(Graphics g, float delay)
         {
-            // g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
             g.Clear(Color.Black);
 
             var redBrush   = new SolidBrush(Color.Red);
@@ -137,10 +231,9 @@ namespace ArrayVisualization
             var w = (float)Width / this.Algorithm.Array.Count;
             var hh = (float)Height / this.Algorithm.Array.Count;
 
-
-            for (int i = 0; i < this.Algorithm.Array.Count; ++i)
+            for (int i = 0; i < this.Array.Count; ++i)
             {
-                var item = this.Algorithm.Array.Elements[i];
+                var item = this.Array.Elements[i];
                 var value = ((NumberElement)item).Value;
 
                 var x = i * w;
@@ -148,8 +241,11 @@ namespace ArrayVisualization
 
                 var h = Height - y;
 
-
                 var brush = whiteBrush;
+                if (this.Colored)
+                {
+                    brush = new SolidBrush(HSL2RGB((double)value / this.Array.Count, 1, 0.5));
+                }
 
                 for (var j = 0; j < lastState.Indices.Count; ++j)
                 {
