da3484d6cb94eec02ddbd6b32a1b8e844c1b7849
HalidOdat <halidodat@gmail.com>
2022-06-28 08:46:18 +0200

    Implement Gnome Sort Algorithm

---
+++ ArrayVisualization/Algorithms/GnomeSortAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayVisualization.Algorithms
+{
+    public class GnomeSortAlgorithm : Algorithm
+    {
+        public GnomeSortAlgorithm(Array array) : base(array)
+        {
+        }
+
+        protected override IEnumerator<AlgorithmState> CreateEnumerator()
+        {
+			for (int i = 1; i < Array.Count;)
+			{
+				yield return new AlgorithmState(new List<int> { i });
+				if (Array[i - 1] <= Array[i])
+				{
+					++i;
+				}
+				else
+				{
+					Array.Swap(i,  i - 1);
+					yield return new AlgorithmState(new List<int> { i });
+
+					--i;
+					if (i == 0) 
+						i = 1;
+				}
+				yield return new AlgorithmState(new List<int> { i });
+			}
+		}
+    }
+}
